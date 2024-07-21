418d81b01fa0b9e8afa85300af75d9de329bf784
KongKarin <karin.npng@gmail.com>
2024-07-21 17:47:28 +0700

    Fix: remove "vercel.js" and dist address,

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { formats: ["image/avif", "image/webp"], minimumCacheTTL: 60 },
-  distDir: ".next",
 };
 
 export default nextConfig;
